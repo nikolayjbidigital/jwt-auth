ea037301cae36be679a144643fd548aa816e9bbc
nikolayjbidigital <nikolay@jbidigital.co.uk>
2016-08-16 16:58:41 +0100

    Update GetUserFromToken.php

--- src/Middleware/GetUserFromToken.php
+++ src/Middleware/GetUserFromToken.php
@@ -25,24 +25,26 @@ class GetUserFromToken extends BaseMiddleware
      */
     public function handle($request, \Closure $next)
     {
+        $token = $this->auth->setRequest($request)->getToken();
         if (! $token = $this->auth->setRequest($request)->getToken()) {
-            return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
+           // return $this->respond('tymon.jwt.absent', 'token_not_provided', 400);
         }
 
         try {
             $user = $this->auth->authenticate($token);
         } catch (TokenExpiredException $e) {
-            return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+            if($token){
+                return $this->respond('tymon.jwt.expired', 'token_expired', $e->getStatusCode(), [$e]);
+            }
         } catch (JWTException $e) {
-            return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+            if($token){
+                 return $this->respond('tymon.jwt.invalid', 'token_invalid', $e->getStatusCode(), [$e]);
+            }
         }
 
-        if (! $user) {
-            return $this->respond('tymon.jwt.user_not_found', 'user_not_found', 404);
+        if($token){
+         $this->events->fire('tymon.jwt.valid', $user);
         }
-
-        $this->events->fire('tymon.jwt.valid', $user);
-
         return $next($request);
     }
 }
